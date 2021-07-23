1fa44811ff2e7270f7fe4c0050671258f34288ff
Michał Stępień <michal.stepien@rignieraxelspringer.pl>
2021-07-23 15:55:04 +0200

    final code for app

--- React/app/src/index.tsx
+++ React/app/src/index.tsx
@@ -1,16 +1,18 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 // import GuestList from './state/GuestLIst';
 // import UserSearch from './state/UserSearch';
-import EventComponeny from './events/EventComponent'
+// import EventComponeny from './events/EventComponent'
+import UserSearch from "./refs/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <EventComponeny/>
+      <UserSearch />
+      {/* <EventComponeny/> */}
       {/* <UserSearch /> */}
       {/* <GuestList /> */}
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
